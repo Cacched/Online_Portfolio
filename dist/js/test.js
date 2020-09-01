e83e9a27f7d9d5f12ca951d9bbfa51ca84d08164
“Cacched” <aadarkv@gmail.com>
2020-08-31 20:38:27 -0500

    1st Revision

--- dist/js/test.js
+++ dist/js/test.js
@@ -5,42 +5,29 @@ const PORT = 5500;
 const app = express();
 app.set("view engine", "jade");
 app.set("views", __dirname + "/views");
-app.use(express.static("/home/aadar/Online_Portfolio/dist" + "/css"));
-app.use(express.static("/home/aadar/Online_Portfolio/dist" + "/img"));
-app.use(express.static("/home/aadar/Online_Portfolio/dist" + "/js"));
+app.use(
+  express.static("/mnt/c/Users/aadsh/Desktop/Online_Portfolio/dist" + "/css")
+);
+app.use(
+  express.static("/mnt/c/Users/aadsh/Desktop/Online_Portfolio/dist" + "/img")
+);
+app.use(
+  express.static("/mnt/c/Users/aadsh/Desktop/Online_Portfolio/dist" + "/js")
+);
 var db = require("pg");
 var dbConnection =
   "postgressql://postgres:Abeeaad112!@localhost:5432/MyProjects";
 
-router.get("/dist/work.html", (req, res) => {
-  //res.sendFile("/home/aadar/Online_Portfolio/dist/work.html");
-  var dbClient = new db.Client(dbConnection);
-
-  dbClient.connect(function (err) {
-    if (err) throw err;
-
-    var query = "select * from projects";
-
-    dbClient.query(query, function (err, result) {
-      if (err) throw err;
-      else {
-        console.log(result.rows[0].name);
-        res.render("files", {
-          files: result.rows,
-          title: "Reading Database",
-        });
-        res.end();
-      }
-    });
-  });
+router.get("/work.html", (req, res) => {
+  res.sendFile("/mnt/c/Users/aadsh/Desktop/Online_Portfolio/dist/work.html");
 });
 
-router.get("/dist/index.html", (req, res) => {
-  res.sendFile("/home/aadar/Online_Portfolio/dist/index.html");
+router.get("/index.html", (req, res) => {
+  res.sendFile("/mnt/c/Users/aadsh/Desktop/Online_Portfolio/dist/index.html");
 });
 
-router.get("/dist/about.html", (req, res) => {
-  res.sendFile("/home/aadar/Online_Portfolio/dist/about.html");
+router.get("/about.html", (req, res) => {
+  res.sendFile("/mnt/c/Users/aadsh/Desktop/Online_Portfolio/dist/about.html");
 });
 
 app.route("/goData").get(function (req, res) {
